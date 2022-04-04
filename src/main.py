a383f06cf5c831b09cea3e52a234b89421debf45
Shashwat <10794178+TheTrio@users.noreply.github.com>
2022-04-04 20:15:44 +0530

    user input improvements

--- src/main.py
+++ src/main.py
@@ -4,9 +4,11 @@ from termcolor import cprint
 if __name__ == "__main__":
     while True:
         line = input("$ ")
+        if line.lower() in ("exit", "quit"):
+            break
         syntax_tree = SyntaxTree.generate(line)
         for error in syntax_tree.errors:
-            cprint(error, "red")  # type: ignore
+            cprint(error, "red", attrs=["bold"])  # type: ignore
         syntax_tree.pretty_print()
         if not syntax_tree.errors:
             print(syntax_tree.evaluate())
